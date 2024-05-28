839a83b1b67a1806da0e8a1c69f70e37192385d3
Tikky <tikkyontheinternets@gmail.com>
2024-05-28 13:45:38 -0600

    initial commit

---
+++ kforms.py
@@ -0,0 +1,10 @@
+
+
+
+"""
+TODOS
+- create a git repo for this file
+- reimplement polynomials but multivariate
+- implement a way to show off and construct k-forms (with integer coefficients)
+
+"""
