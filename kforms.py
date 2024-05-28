bc8dcfe36410c56e5b5f101e42c8293156e04571
Tikky <tikkyontheinternets@gmail.com>
2024-05-28 14:11:25 -0600

    added a bunch of empty functions to implement later

--- kforms.py
+++ kforms.py
@@ -9,4 +9,55 @@ TODOS
 
 """
 
-# the file just got modified :3
+class MultiPoly:
+    def __init__(num_variables):
+        self.num_variables = num_variables
+        self.coefficients = [0]
+        self.degree = 0
+
+    # TODO
+    def set_coef(value,exponents):
+        pass
+    def get_coef(exponents):
+        return 0
+
+    # TODO
+    def copy():
+        pass
+
+    # operates in place
+    # multiplies by other poly
+    # TODO
+    def mul_poly(poly):
+        pass
+    def add_poly(poly):
+        pass
+
+    def mul_const(const):
+        pass
+        # must update degree potentially
+
+class KForm:
+    def __init__(num_variables):
+        self.num_variables = num_variables
+        self.zero_forms = [0]
+    
+    # TODO
+    # zero_form: whatever function type we're working with
+    # term: an array of nonnegative integers, the differentials in this term
+    def set_zero_form(zero_form,term):
+        pass
+    def get_zero_form(term):
+        return 0
+
+# TODO
+# return the index in a multipoly array that a given term would be located at
+# poly_term: an array of positive integers, the exponents of the variables
+def poly_term_index(poly_term):
+    return 0
+
+# TODO
+# return the index in a k-form array that a given term would be located at
+# kform_term: an array of nonnegative integers (the variables in this differential)
+def kform_term_index(kform_term):
+    return 0
